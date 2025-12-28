2e2a113704005b57602f6e5ac8fd84835e22ef71
Sam <sam.oystreck@gmail.com>
2025-12-28 16:06:21 -0400

    Got initial scraper to work

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A program designed to analyze Warhammer 40k combat, specifically regarding expected wounds dealt or taken.
 
 Version 1.0 implementation will simply scrape Wahapedia for unit stats and compare simulated and expected combat between two units, however future versions may be develloped to include stratagem use, unit-specific abilities and other game-altering rules that are outside the scope of weapon profiles.
+
+Types of queries  we want:
+How will this unit with # models do against this unit with this many models (optional distance, wounds, wargear(?))
+
+# Scraping and re-scraping:
--- webscraper.py
+++ webscraper.py
@@ -2,39 +2,44 @@ from bs4 import BeautifulSoup
 import requests
 from dotenv import load_dotenv
 import os
-from selenium import webdriver
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#To rescrape if the site is changed add "view-source:(INSERT URL)" and reexamine html
+# ^^ Will eventually go in a rescraping file
 
-#Code currently not functional
-#Wahapedia injects the headers we are attempting to scrape but pulls information from
-#a Factions.csv file that we ARE able to scrape
-
-#To-do : Find out how to identify this (How to see that a webpage is injecting content
-# that we are looking for), as well as how to find the information that we can actually use
+# As of 2025-12-28 the current html hierarchy of the factions is as follows:
+#                                            <NavColumns2 div>
+#                                                 V V V
+#                                          <BreakInsideAvoid div>
+#                                                 V V V
+# <FactionHeader div>Faction Name 1</div> <a href="">Subfaction Name 1</a> <a href="">Subfaction Name 2</a> ...
+# <FactionHeader div>Faction Name 2</div> <a href="">Subfaction Name 1</a> <a href="">Subfaction Name 2</a> ...
+#                                                  ...
+#                                                 </div>
+#                                                 </div>
 
 load_dotenv()
+raw_init_html = requests.get(os.getenv("SITE_INIT"))
+clean_init_html = BeautifulSoup(raw_init_html.text, "lxml")
+main_div = clean_init_html.find("div", class_="NavColumns2")
+faction_types = main_div.findChildren("div", class_= "BreakInsideAvoid")
 
-driver = webdriver.Chrome()
-driver.get(os.getenv("SITE_INIT"))
-
-WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "FactionHeader"))
-)
-
-raw_init_html = driver.page_source
-clean_init_html = BeautifulSoup(raw_init_html, "lxml")
-
-divs = clean_init_html.find_all("div", class_="FactionHeader")
-print(divs)
+#Making a list of all the factions that we will need to scrape with links (currently href)
+# to them
 factions = []
+for faction_type in faction_types:
+    subfactions = faction_type.find_all("a", recursive = False)
+    faction_name = faction_type.find("div", class_="FactionHeader").get_text()
+    for subfaction in subfactions:
+        name = subfaction.getText()
+        href = subfaction.get("href")
+        factions.append({"Faction" : faction_name, "Subfaction" : name, "href" : href})
 
+print(factions)
 
-driver.quit()
-
-#Need to make a list of all the pages to scrape (all faction pages) from https://wahapedia.ru/wh40k10ed/the-rules/quick-start-guide/
+#Endgoal : list containing every weapon profile for every unit of every faction with ways to identify:
+#   - Superfaction
+#   - Faction
+#   - Model
 
 #Then from each faction page we need to make a list of all model pages to scrape
 
